f31041a7e3502304ae14cc0cfd095300ab026b0c
tgextreme <tgextreme89@gmail.com>
2022-05-23 13:04:27 +0200

    comit

--- tabla.php
+++ tabla.php
@@ -6,7 +6,19 @@ $json=file_get_contents("Monumentos.json");
 //var_dump($json);
 $json = json_decode(preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $json), true);
 //print_r ($json);
+$nombre =$_GET['query'];
+
 foreach ($json as $j){
+    $patronNombre=preg_match("/".$nombre."/", $j["Nombre"]);
+    $patronMunicipio=preg_match("/".$nombre."/", $j["Municipio"]);
+    @$patronDescripcin=preg_match("/".$nombre."/", $j["Descripcin"]);
+    $patronCp=preg_match("/".$nombre."/", $j["C.P."]);
+    if(
+        $patronNombre==1 or
+        $patronMunicipio==1 or
+        $patronDescripcin==1 or
+        $patronCp==1
+    ){
     echo "<tr>";
     echo "<td>";
     echo $j["Nombre"];
@@ -23,5 +35,5 @@ foreach ($json as $j){
     
     echo "</tr>";
 }
+}
 echo "</table>";
-?>
